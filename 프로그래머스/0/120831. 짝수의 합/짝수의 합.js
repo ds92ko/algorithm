1e51c6181bbbe1bf1eee13baf8803010769753ba
Dasom Ko <ds92ko@gmail.com>
2025-07-11 14:20:17 +0900

    [level 0] Title: 짝수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,4 +1,4 @@
-const solution = n => Array.from({ length: n }).reduce((acc, cur, idx) => {
+const solution = n => Array(n).fill().reduce((acc, cur, idx) => {
     cur = idx + 1;
     return cur % 2 === 0 ? acc + cur : acc;
 }, 0);

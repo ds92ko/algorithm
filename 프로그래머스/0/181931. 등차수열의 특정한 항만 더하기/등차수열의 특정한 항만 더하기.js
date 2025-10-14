893fe5eb861a329f4f97975b20b34eba30bcc79e
Dasom Ko <ds92ko@gmail.com>
2025-10-14 16:25:24 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,4 +1,2 @@
-const solution = (a, d, included) => included.reduce((acc, cur, idx) => {
-    if (cur) acc += !idx ? a : a + d * idx;
-    return acc;
-}, 0);
+const solution = (a, d, included) => included.reduce((acc, cur, idx) => 
+    cur ? acc + a + d * idx : acc, 0);
